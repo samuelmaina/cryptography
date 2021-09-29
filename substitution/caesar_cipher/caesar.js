a4ef97e972b0c69c8493536a59d2c8078aa44898
samuelmayna <samuelmayna@gmail.com>
2021-09-29 20:05:55 +0300

    Added test and functionality for decryption  of plain text

---
+++ substitution/caesar_cipher/caesar.js
@@ -0,0 +1,21 @@
+//ascii character codes ranges.
+const upperlimit = 126;
+const lowerlimit = 32;
+
+exports.encryptString = (string, interval) => {
+  const len = string.length;
+  let result = "";
+  let charCode;
+  for (let i = 0; i < len; i++) {
+    charCode = string.charCodeAt(i);
+    const encyptedChar = encryptChar(charCode, interval);
+    result += String.fromCharCode(encyptedChar);
+  }
+  return result;
+};
+
+function encryptChar(charCode, interval) {
+  let remainder = (charCode + interval) % upperlimit;
+  if (remainder < lowerlimit) return (charCode += lowerlimit);
+  return remainder;
+}
--- substitution/caesar_cipher/caesar.test.js
+++ substitution/caesar_cipher/caesar.test.js
@@ -8,11 +8,38 @@ describe("Should be able to encrypt", () => {
     expect(encryptString(plain, interval)).toBe(ciphertext);
   });
   describe("should encyrpt single characters", () => {
-    it.only("single small letters", () => {
+    it("lowercase", () => {
       const plain = "a";
       const interval = 3;
       const ciphertext = "d";
       expect(encryptString(plain, interval)).toBe(ciphertext);
+    });
+    it("uppercase", () => {
+      const plain = "A";
+      const interval = 7;
+      const ciphertext = "H";
+      expect(encryptString(plain, interval)).toBe(ciphertext);
+    });
+    it("characters", () => {
+      const plain = "?";
+      const interval = 5;
+      const ciphertext = "D";
+      expect(encryptString(plain, interval)).toBe(ciphertext);
+    });
+  });
+
+  describe("should encrypt for strings", () => {
+    it("for small strings", () => {
+      const plain = "JohnDoe34?";
+      const interval = 5;
+      const ciphertext = "OtmsItj89D";
+      expect(encryptString(plain, interval)).toBe(ciphertext);
+    });
+    it.only("for long strings", () => {
+      const plain = "ABC, abc, 123 and @#$#&";
+      const interval = 5;
+      const ciphertext = "FGH1%fgh1%678%fsi%E()(+";
+      expect(encryptString(plain, interval)).toEqual(ciphertext);
     });
   });
 });
